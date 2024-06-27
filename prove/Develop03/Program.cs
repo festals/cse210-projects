1f12f5278de7b5adb67811c29775316ee1267443
festals <festals90@gmail.com>
2024-06-27 11:33:28 +0200

    add ref scri word and ther att and const

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string answer;
+
+        do
+        {
+            
+            Console.Write("Press enter to continue or type 'quit' to finish: ");
+            answer = Console.ReadLine();
+            
+            string key = Console.ReadKey().Key.ToString();
+            do  // while they press enter (i think im not sure that work)
+            {
+
+            } while (key.ToUpper() == "");
+
+        } while(answer != "quit"); //or there is no more word
     }
 }
