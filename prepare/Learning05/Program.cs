2888a37ca071318c9ad328e96795402896197ca2
festals <festals90@gmail.com>
2024-07-08 16:40:35 +0200

    add shape

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("blue", 2);
+        Rectangle r1 = new Rectangle("yellow",3,6);
+        Circle c1= new Circle("purple", 5);
+
+        // Console.WriteLine(square1.GetArea());
+        // Console.WriteLine(square1.GetColor());
+
+        // Console.WriteLine(r1.GetArea());
+        // Console.WriteLine(r1.GetColor());
+
+        // Console.WriteLine(c1.GetArea());
+        // Console.WriteLine(c1.GetColor());
+
+        shapes.Add(square1);
+        shapes.Add(r1);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"{color} - {area}");
+        }
+
+
     }
 }
